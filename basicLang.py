1ba6a63ad9617f4bb1b1778b229a657fb3a9abea
WilliamStarling <williamjonas@comcast.net>
2024-11-29 04:15:25 -0600

    Added power function
    Added the ability to set numbers to be "to the power pf" other numbers, using ^

--- basicLang.py
+++ basicLang.py
@@ -12,6 +12,7 @@ TT_PLUS     = 'PLUS'
 TT_MINUS    = 'MINUS'
 TT_MUL      = 'MUL'
 TT_DIV      = 'DIV'
+TT_POW		= 'POW'
 TT_LPAREN   = 'LPAREN'
 TT_RPAREN   = 'RPAREN'
 TT_EOF		= 'EOF'
@@ -154,6 +155,9 @@ class Lexer:
 			elif self.current_char == '/': #checks if it's /
 				tokens.append(Token(TT_DIV, pos_start=self.pos))
 				self.advance()
+			elif self.current_char == '^': #checks if it's /
+				tokens.append(Token(TT_POW, pos_start=self.pos))
+				self.advance()
 			elif self.current_char == '(': #checks for (
 				tokens.append(Token(TT_LPAREN, pos_start=self.pos))
 				self.advance()
@@ -273,17 +277,12 @@ class Parser:
 			return res.failure(InvalidSyntaxError(self.current_tok.pos_start, self.current_tok.pos_end, "Expected '+', '-', '*' or '/'"))
 		return res
 
-	def factor(self):
+#######################
+	def atom(self):
 		res = ParseResult()
-		tok = self.current_tok #gets the current token
+		tok = self.current_tok
 
-		if tok.type in (TT_PLUS, TT_MINUS): #checks for unary character to make number pos or neg.
-			res.register(self.advance())
-			factor = res.register(self.factor())
-			if res.error: return res
-			return res.success(UnaryOpNode(tok, factor))
-		
-		elif tok.type in (TT_INT, TT_FLOAT): #checks if token is an integer or a float
+		if tok.type in (TT_INT, TT_FLOAT): #checks if token is an integer or a float
 			res.register(self.advance())
 			return res.success(NumberNode(tok))
 
@@ -297,7 +296,24 @@ class Parser:
 			else:
 				return res.failure(InvalidSyntaxError(self.current_tok.pos_start, self.current_tok.pos_end, "Expected ')'"))
 
-		return res.failure(InvalidSyntaxError(tok.pos_start, tok.pos_end, "Expected int or float")) #returns a failure, because it didn't get an int or float.
+		return res.failure(InvalidSyntaxError(
+      		tok.pos_start, tok.pos_end, "Expected int, float, '+', '-', or '('"
+        )) #returns a failure, because it didn't get an int or float.
+  
+	def power(self):
+		return self.bin_op(self.atom, (TT_POW, ), self.factor)
+
+	def factor(self):
+		res = ParseResult()
+		tok = self.current_tok #gets the current token
+
+		if tok.type in (TT_PLUS, TT_MINUS): #checks for unary character to make number pos or neg.
+			res.register(self.advance())
+			factor = res.register(self.factor())
+			if res.error: return res
+			return res.success(UnaryOpNode(tok, factor))
+
+		return self.power()
 
 	def term(self):
 		return self.bin_op(self.factor, (TT_MUL, TT_DIV)) #Gets a term by looking for factors, and then the multiplication or division operator.
@@ -305,15 +321,18 @@ class Parser:
 	def expr(self):
 		return self.bin_op(self.term, (TT_PLUS, TT_MINUS)) #gets an expression by looking for terms, and then the addition or subtraction operator.
 
-	def bin_op(self, func, ops): #Looks for the specified function (either factor or term), and then looks for the specified operators.
+	def bin_op(self, func_a, ops, func_b=None): #Looks for the specified function (either factor or term), and then looks for the specified operators.
+		if func_b == None:
+			func_b = func_a
+   
 		res = ParseResult()
-		left = res.register(func())
+		left = res.register(func_a())
 		if res.error: return res
 
 		while self.current_tok.type in ops:
 			op_tok = self.current_tok
 			res.register(self.advance())
-			right = res.register(func())
+			right = res.register(func_b())
 			if res.error: return res
 			left = BinOpNode(left, op_tok, right)
 
@@ -380,6 +399,10 @@ class Number:
 
 			return Number(self.value / other.value).set_context(self.context), None
 
+	def powed_by(self, other):
+		if isinstance(other, Number):
+			return Number(self.value ** other.value).set_context(self.context), None #python power operator is **
+
 	def __repr__(self):
 		return str(self.value)
 
@@ -427,6 +450,8 @@ class Interpreter:
 			result, error = left.multed_by(right)
 		elif node.op_tok.type == TT_DIV: #if it's a divide, divide them
 			result, error = left.dived_by(right)
+		elif node.op_tok.type == TT_POW: #if it's a divide, divide them
+			result, error = left.powed_by(right)
 
 		if error:
 			return res.failure(error)
